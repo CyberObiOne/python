623100e3968d0cb4e706839c58058f4751375502
RFiKT <hiva.vlad@gmail.com>
2020-03-04 15:23:15 +0300

    Update orphaned_confluence_slack_jira_notifications.py

--- project_archive/confluence/orphaned_confluence_slack_jira_notifications.py
+++ project_archive/confluence/orphaned_confluence_slack_jira_notifications.py
@@ -20,14 +20,14 @@ jira = JIRA(basic_auth=(os.environ.get("JIRA_LOGIN"), os.environ.get("JIRA_PASS"
 if   orphaned_spaces: #json is not empty
         scope_of_projects = ''
         for spaces in inactive_projects:
-# I have two extra loops, because I didn't find a way to convert jsom from sql in right format
+# I have two extra loops, because I didn't find a way to convert json from sql in right format
                 for i in spaces:
                         for a in i:
                                 summary =  a['spacename'] + ' '  + 'Space Archive'
                                 search_string = "summary ~\"{}\" and statuscategory!=done ".format(summary)
                                 issues = jira.search_issues(search_string, maxResults=25)
-                                if issues: #if issue already exist on jira server - add a comment instead of creation.
-                                        jira.add_comment(issues[0].key, "Please, provide an update [~{}]".format (HEADERS['assignee']['name']))
+                                if issues: #if issue already exist on jira server - ignore creation.
+                                        break
                                 else:
 
                                         url_spacename = urllib.quote_plus( a['spacename'])
@@ -38,6 +38,6 @@ if   orphaned_spaces: #json is not empty
                                         new_issue = jira.create_issue(fields=issue_dict) # create new jira issue with predifined variables
                                         scope_of_projects += a['spacename'] + ' - ' +  'https://jira_url.com/jira/browse/'  + new_issue.key  + '\n' #generate payload to Slack
                                         print (new_issue.key)
-        if scope_of_projects: #if strin is not empty - send notification to slack. 
+        if scope_of_projects: #if string is not empty - send notification to slack. 
                 payload = {'text': "Orphaned Confleunce Space(s) without activity for 180 days: \n {} ".format(scope_of_projects) }
                 r = requests.post("https://hooks.slack.com/services/channel/incomming/webhook_here", data=json.dumps(payload))
