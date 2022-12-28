6d57e5b4917e5e1d5604e51ec26f6f11da8f45f9
bro-n-bro-2 <89855562+bro-n-bro-2@users.noreply.github.com>
2022-12-28 15:03:37 +0100

    Update workload_report.py

--- reports/workload_report.py
+++ reports/workload_report.py
@@ -3,16 +3,24 @@ from requests.auth import HTTPBasicAuth
 import json
 from jira import JIRA
 import csv
-jira = JIRA(basic_auth=("email", "token"), options={'server': 'Jira_URL', 'verify': True})  # Jira Cloud: a username/token tuple
-for issue in jira.search_issues('project = Test AND sprint in (1, 2, 3, 4)', maxResults=400):
+jira = JIRA(basic_auth=("email", "API Token"), options={'server': 'Jira_URL', 'verify': True})  # Jira Cloud: a username/token tuple
+for issue in jira.search_issues('project = MAG AND sprint in (7345) ', maxResults=400):
     issues = jira.issue(issue.key)
     if issues.fields.worklog.worklogs !=0:
-        data = []
+        data_comment = []
+        data_empty_comment = []
         for worklog in issues.fields.worklog.worklogs:
-            data.append([issue.key, issue.fields.summary,worklog.author.displayName,worklog.comment.replace("\n","").replace("\t","").replace("\r",""),worklog.timeSpent])
-            #print (data)
+            if hasattr(worklog, 'comment'):
+                #print(issue.key)
+                data_comment.append([issue.key, issue.fields.summary, worklog.author.displayName,  worklog.timeSpent ,worklog.comment.replace("\n", "").replace("\t", "").replace("\r", "")])
+
+            else:
+                #print('No comment ' + issue.key)
+                data_empty_comment.append([issue.key, issue.fields.summary, worklog.author.displayName,  worklog.timeSpent] )
+            print (data_comment,data_empty_comment)
         with open('report.csv', 'a', encoding='UTF8', newline='') as f:
             writer = csv.writer(f, delimiter=',')
-            header = ['key', 'summary', 'author', 'comment', 'timeSpent']
+            header = ['key', 'summary', 'author', 'timeSpent', 'comment']
             writer.writerow(header)
-            writer.writerows(data)
+            writer.writerows(data_comment)
+            writer.writerows(data_empty_comment)
